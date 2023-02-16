bfff1ff611660ed16e98c60f21138ff908b3d797
aliveness-zp <81018609+aliveness-zp@users.noreply.github.com>
2023-02-16 17:00:03 +0200

    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const LOCALKEY = 'feedback-form-state';
+
+const formData = {};
+const parsedData = JSON.parse(localStorage.getItem(LOCALKEY));
+
+const formEl = document.querySelector('form');
+const textAreaEl = document.querySelector('textarea');
+const inputEl = document.querySelector('input');
+
+formEl.addEventListener('input', throttle(createLocalStorage, 500));
+formEl.addEventListener('submit', clearAndSubmiForm);
+
+reloadFormValue();
+
+function createLocalStorage(e) {
+  formData[e.target.name] = e.target.value;
+
+  return localStorage.setItem(LOCALKEY, JSON.stringify(formData));
+}
+
+function clearAndSubmiForm(e) {
+  e.preventDefault();
+
+  console.log(parsedData);
+
+  e.target.reset();
+
+  localStorage.removeItem(LOCALKEY);
+}
+
+function reloadFormValue() {
+  if (parsedData) {
+    textAreaEl.value = parsedData.message || '';
+    inputEl.value = parsedData.email || '';
+  }
+}
